67b2a929643497fc322ed35d027c9c7b200b0ecc
UnJavaScripter <eltu.khan@gmail.com>
2016-01-24 18:26:27 -0500

    Updated dist files

--- app/components/data-service/data-service.js
+++ app/components/data-service/data-service.js
@@ -24,8 +24,8 @@ System.register(['angular2/core', 'angular2/http'], function(exports_1) {
                     this.http = http;
                 }
                 DataService.prototype.getData = function () {
-                    //const URL = 'https://raw.githubusercontent.com/UnJavaScripter/endpoint-doc/gh-pages/src/_endpoint_data_.json';
-                    var URL = '../../../_endpoint_data_.json';
+                    var URL = 'https://raw.githubusercontent.com/UnJavaScripter/endpoint-doc/master/src/_endpoint_data_.json';
+                    //const URL = '../../../_endpoint_data_.json';
                     // return an observable
                     return this.http.get(URL)
                         .map(function (res) { return res.json(); });
--- app/components/data-service/data-service.ts
+++ app/components/data-service/data-service.ts
@@ -9,8 +9,8 @@ export class DataService {
     }
 
     getData() {
-        //const URL = 'https://raw.githubusercontent.com/UnJavaScripter/endpoint-doc/gh-pages/src/_endpoint_data_.json';
-        const URL = '../../../_endpoint_data_.json';
+        const URL = 'https://raw.githubusercontent.com/UnJavaScripter/endpoint-doc/master/src/_endpoint_data_.json';
+        //const URL = '../../../_endpoint_data_.json';
        
         // return an observable
         return this.http.get(URL)
